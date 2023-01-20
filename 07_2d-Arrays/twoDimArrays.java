afdd8c48198958402a8c88d21c75b2a73c0bf573
zain sattar <zainsattar17@gmail.com>
2023-01-21 00:52:48 +0500

    2D arrays ,lec#23

--- 07_2d-Arrays/twoDimArrays.java
+++ 07_2d-Arrays/twoDimArrays.java
@@ -28,6 +28,50 @@ public class twoDimArrays {
         }
         return res;
 	}
+    //Problem 46: Print in spiral
+    public static void spiralPrint(int matrix[][]){
+        //if matrix is empty
+        if(matrix.length==0){
+            return ;
+        }
+		int firstRow=0;
+        int firstCol=0;
+        int LastRow=matrix.length-1;
+        int LastCol=matrix[0].length-1;
+        int size=matrix.length*matrix[0].length;
+
+        //int res[]=new int[size];
+        int count=0;
+        while(count<size){
+            //printFirstRow (forward printing)
+            for(int j=firstCol;j<=LastCol && count<size ;j++){
+                System.out.print(matrix[firstRow][j]+" ");
+                count++;
+            }
+            firstRow++;
+
+            //printLastCol (forward printing)
+            for(int j=firstRow;j<=LastRow && count<size ;j++){
+                System.out.print(matrix[j][LastCol]+" ");
+                count++;
+            }
+            LastCol--;
+
+            //printLastRow (reverse printing)
+            for(int j=LastCol;j>=firstCol && count<size ;j--){
+                System.out.print(matrix[LastRow][j]+" ");
+                count++;
+            }
+            LastRow--;
+
+            //printFirstCol (reverse printing)
+            for(int j=LastRow;j>=firstRow && count<size ;j--){
+                System.out.print(matrix[j][firstCol]+" ");
+                count++;
+            }
+            firstCol++;
+        }
+	}
 
     public static void printoneDimArray(int []arr){
         for (int i = 0; i < arr.length-1; i++) {
@@ -37,8 +81,9 @@ public class twoDimArrays {
     }
     
     public static void main(String[] args) {
-        int [][]arr={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        int a1[]=wavePrint(arr, 3, 4);
+        int [][]arr={{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        int a1[]=wavePrint(arr, 4, 3);
         printoneDimArray(a1);
+        spiralPrint(arr);
     }
 }
